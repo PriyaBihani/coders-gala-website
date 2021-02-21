5d576d1820215dacb4e9a78a5877f8046e8d0c6b
kartik <gupta.kartik18kg@gmail.com>
2021-02-21 15:35:00 +0530

    about sections

---
+++ client/src/sections/about/TopSlide.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const TopSlide = () => {
+  return (
+    <div className="top-slide">
+      <div className="ovr-wave">
+        <h1>Hello there,</h1>
+
+        <div className="subheading-about">
+          We created <span className="logo text-dark">Coders Gala</span> to
+          provide You Quality resources to learn From
+        </div>
+        <div>
+          <img
+            alt="scroll icon"
+            src="https://gifimage.net/wp-content/uploads/2018/05/scroll-gif-10.gif"
+            className="scroll-icon"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopSlide;

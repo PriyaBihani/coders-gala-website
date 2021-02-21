62604df4e79de55a0b293644547ebce9c0463776
PriyaBihani <priyabihani.official@gmail.com>
2021-02-21 14:45:42 +0530

    intial commit

---
+++ server/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+var app = require('./app');
+var http = require('http');
+
+var server = http.createServer(app);
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = process.env.SERVICE_PORT || '3001';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', () => {
+  console.log('ERROR');
+});
+server.on('listening', () => {
+  console.log(`listening on port ${port}`);
+});
+
+/**
+ * Normalize a port into a number, string, or false.
+ */

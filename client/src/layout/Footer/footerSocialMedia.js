d1aa9af15324af31e34b762effbb8de6cc23a2fd
PriyaBihani <priyabihani.official@gmail.com>
2021-02-21 15:37:00 +0530

    Layout

---
+++ client/src/layout/Footer/footerSocialMedia.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+import React from "react";
+
+const FooterSocialMedia = () => {
+  return (
+    <div className="social-media">
+      <a rel="nofollow" href="#">
+        <img
+          src="https://www.svgrepo.com/show/303161/gmail-icon-logo.svg"
+          alt=""
+        />
+      </a>
+      <a rel="nofollow" href="#">
+        <img src="https://www.svgrepo.com/show/183608/twitter.svg" alt="" />
+      </a>
+      <a rel="nofollow" href="#">
+        <img src="https://www.svgrepo.com/show/111199/instagram.svg" alt="" />
+      </a>
+      <a rel="nofollow" href="#">
+        <img
+          src="https://www.svgrepo.com/show/217753/github.svg"
+          className="github"
+          alt=""
+        />
+      </a>
+    </div>
+  );
+};
+
+export default FooterSocialMedia;

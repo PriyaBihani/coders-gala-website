e5289ffb5bc6cab6b520bd428be044283e436a5a
PriyaBihani <priyabihani.official@gmail.com>
2021-03-02 11:16:04 +0530

    fix

--- client/src/layout/Navbar/SignedInLinks.js
+++ client/src/layout/Navbar/SignedInLinks.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext } from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const SignedInLinks = () => {
   return (
